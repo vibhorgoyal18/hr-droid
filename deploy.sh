abed300770b788d987743146ebff27ae066f3f3b
VIBHOR GOYAL <goyal1992.vibhor@gmail.com>
2019-06-25 22:20:00 +0530

    Updated build script

--- deploy.sh
+++ deploy.sh
@@ -105,8 +105,8 @@ echo Handling node.js deployment.
 selectNodeVersion
 
 # 3. Install npm packages
-if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
-  cd "$DEPLOYMENT_TARGET"
+if [ -e "$DEPLOYMENT_SOURCE/package.json" ]; then
+  cd "$DEPLOYMENT_SOURCE"
   echo "Running $NPM_CMD install --production"
   eval $NPM_CMD install --production
   exitWithMessageOnError "npm failed"
